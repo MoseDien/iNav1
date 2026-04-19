259c9f925adfdbde9319b8a3d1ef52b0aa77a993
Bell <dienbell@gmail.com>
2026-04-19 16:08:06 +0800

    Initial Commit

---
+++ iNav1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iNav1
+//
+//  Created by bell dien on 19/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iNav1/iNav1App.swift
@@ -0,0 +1,17 @@
+//
+//  iNav1App.swift
+//  iNav1
+//
+//  Created by bell dien on 19/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct iNav1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
